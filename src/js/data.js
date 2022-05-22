4d6c03a1f131f630cb6b5fff437f03535de55d54
Viktor <88923141+Viktor-Streltsov@users.noreply.github.com>
2022-05-22 22:17:18 +0600

    Add files via upload

---
+++ src/js/data.js
@@ -0,0 +1,27 @@
+const data = {
+    "first_name": "Viktor",
+    "last_name": "Streltsov",
+    "job_title": "Front-end Development",
+    "photo": "images/I.jpg",
+    "phone": "+966-557-501-017",
+    "email": "Vik.O.r_23@proton.me",
+    "GIThub": "https://github.com/Viktor-Streltsov/Projects/blob/main/ProjectMag/components/index.js",
+    "linkedin": "Not_the_Morty",
+    "address": "КР г.Бишкек д.49",
+    "skills": [
+        "HTML",
+        "CSS(SCSS, Lees)",
+        "JavaScript",
+        "TypeScript",
+        "React/Redux",
+        "OOP(Rest,API)",
+        "Github, Gitlab",
+    ],
+    "education": [
+        ["UniverKyrgyz State Technical University named after Isak Razzakov"],
+    ],
+    "achievements": [
+        "Completed courses in HTML, CSS, JS 2021-2022",
+    ],
+    "profile": `I welcome you to my page! Thanks for reading my portfolio. My name is Viktor Vladimirovich and I'm a junior JavaScript, TypeScript, and the React Framework. Successfully completed ItGit courses in 2022. I am currently looking for a job as a Front End Developer. I speak English at Intermedia+ level. Personal qualities: punctuality, sociability, friendliness, fast learner, I easily find a common language with people, I always deal with difficulties calmly and quickly find a solution to the problem. I will be very happy to work in your team.`
+};
---
+++ src/js/experience.js
@@ -0,0 +1,26 @@
+const experience = [
+    {
+        "company": "KSTU University",
+        "city": "Bishkek",
+        "from": "2021",
+        "to": 'Present',
+        "position": "Engineer",
+        "description": "I performed engineering and mathematical calculations, and also visualized them on graphs in the Matlab programming language. I checked the reliability of transformers, autotransformers and their switches for the normal operation of the equipment. Modeled the link layer of a communication system in the 'Simulink' package of the 'Matlab' system."
+    },
+    {
+        "company": "GLOBUS",
+        "city": "Bishkek",
+        "from": "2020",
+        "to": 'Present',
+        "position": "Merchandiser",
+        "description": "During his work in the company 'Globus' he proved to be sociable and able to find a common language with any person. Was recommended as a 'responsible' seller, selling quickly and efficiently. Robots always came to the appointed time, always did their work as quickly and efficiently as possible, so as not to miss deadlines. I checked the products for quality and identified inappropriate products according to the dates of production."
+    },
+    // {
+    //     "company": "COMPANY NAME 2",
+    //     "city": "London",
+    //     "from": "2013",
+    //     "to": '2014',
+    //     "position": "your position",
+    //     "description": "Replace with your key responsibilities, accomplishments and skills that may relevant to the position you are applying for Include the keywords from the listing to capture the employer’s attention and to match applicant tracking system requirements Transform your responsibilities into quantifiable achievements by brainstorming for any numbers relevant to your key duties"
+    // }
+]
