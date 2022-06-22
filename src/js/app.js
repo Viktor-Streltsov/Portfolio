262726fdf758893b5260b90b823d507d2b2f8d7f
Viktor <88923141+Viktor-Streltsov@users.noreply.github.com>
2022-06-22 19:11:15 +0600

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -22,10 +22,9 @@ document.querySelector('.experience-list').innerHTML = expirienceData;
  */
 
 document.querySelector('.contacts-block').innerHTML += `<li><a href="tel:${data.phone}">${data.phone}</a></li>`
-document.querySelector('.contacts-block').innerHTML += `<span><a href="mailto:${data.email}">Email </a></span>`
-document.querySelector('.contacts-block').innerHTML += `<span><a href="${data.GIThub}">🠔 GitHub ➜</a></span>`
-document.querySelector('.contacts-block').innerHTML += `<span><a href="https://t.me/${data.linkedin}"> Telegram</a></span>`
-document.querySelector('.address').innerHTML = `<p>${data.address}</p>`;
+document.querySelector('.contacts-block .line-text').innerHTML += `<span><a href="mailto:${data.email}">Email </a></span>`
+document.querySelector('.contacts-block .line-text').innerHTML += `<span><a href="${data.GIThub}">🠔 GitHub ➜</a></span>`
+document.querySelector('.contacts-block .line-text').innerHTML += `<span><a href="https://t.me/${data.linkedin}"> Telegram</a></span>`
 document.querySelector('#my-photo').src = data.photo;
 
 /**
