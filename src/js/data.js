75e483615bb3f7047b7c3432114474a4963f683c
Viktor <88923141+Viktor-Streltsov@users.noreply.github.com>
2023-04-29 12:33:50 +0600

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -25,5 +25,5 @@ const data = {
     "achievements": [
         ["Completed courses in HTML, CSS, JS 2021-2022"],
     ],
-    "profile": "I welcome you to my page! Thanks for reading my portfolio. My name is Viktor Vladimirovich and I'm a junior JavaScript, TypeScript, and the React Framework. I have 1.5+ years of experience working with these technologies. Successfully completed ItGit courses in 2022. I am currently looking for a job as a Front End Developer. I speak English at Intermedia+ level. Personal qualities: punctuality, sociability, friendliness, fast learner, I easily find a common language with people, I always deal with difficulties calmly and quickly find a solution to the problem. I will be very happy to work in your team."
+    "profile": "I welcome you to my page! Thanks for reading my portfolio. My name is Viktor Vladimirovich and I'm a junior JavaScript, TypeScript, and the React Framework. I have 2 years of experience working with these technologies. Successfully completed ItGit courses in 2022. I am currently looking for a job as a Front End Developer. I speak English at Intermedia+ level. Personal qualities: punctuality, sociability, friendliness, fast learner, I easily find a common language with people, I always deal with difficulties calmly and quickly find a solution to the problem. I will be very happy to work in your team."
 };
