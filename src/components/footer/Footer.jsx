0f0c3802f25c90db045f1c3a587a177e9fe94386
Viktor <viktor4strlsov@gmail.com>
2026-03-18 21:06:27 +0600

    main Стили

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
     <footer className={styles.container}>
       <div>2023 MyApp. All rights reserved</div>
       <div className={styles.social}>
-          <Link rel="icon" href="/favicon.ico" ><Image src='/vk.png' width={15} height={15} className={styles.icon} alt="vk.com"/></Link>
-          <Link rel="icon" href="/favicon.ico" ><Image src="/inst.png" width={15} height={15} className={styles.icon} alt="instagram"/></Link>
-          <Link rel="icon" href="/favicon.ico" ><Image src="/twitter.png" width={15} height={15} className={styles.icon} alt="twitter"/></Link>
-          <Link rel="icon" href="/favicon.ico" > <Image src="/yt.png" width={15} height={15} className={styles.icon} alt="youtube"/></Link>
+          <Link rel="icon" href="https://vk.com/id658913692" ><Image src='/vk.png' width={15} height={15} className={styles.icon} alt="vk.com"/></Link>
+          <Link rel="icon" href="https://www.instagram.com/viktor_str23?igsh=a25zMzN3bzByaDd4&utm_source=qr" ><Image src="/inst.png" width={15} height={15} className={styles.icon} alt="instagram"/></Link>
+          <Link rel="icon" href="/favicon.ico" ><Image src="/telegram.png" width={15} height={15} className={styles.icon} alt="telegram"/></Link>
+          <Link rel="icon" href="/favicon.ico" > <Image src="/linkedin.png" width={15} height={15} className={styles.iconLink} alt="linkedin"/></Link>
 
       </div>
     </footer>
